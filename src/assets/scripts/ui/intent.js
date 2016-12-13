a26d2fd92e71995f347548dd0e7eb067b148c285
Carles Muiños <carles.ml.dev@gmail.com>
2016-12-13 18:14:30 +0100

    build base layout

--- src/assets/scripts/ui/intent.js
+++ src/assets/scripts/ui/intent.js
@@ -1,7 +1,15 @@
-/* eslint-disable no-unused-vars */
 import $ from 'jquery'
 
 export const intent = {
   setup () {
+    const $sidebar = $('.ui.sidebar')
+
+    $sidebar
+      .sidebar({
+        transition: 'overlay',
+        mobileTransition: 'overlay'
+      })
+      .sidebar('attach events', '.pusher .content.icon')
+      .sidebar('attach events', '.ui.sidebar .remove.icon')
   }
 }
