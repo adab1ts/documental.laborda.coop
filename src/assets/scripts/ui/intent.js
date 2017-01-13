8d966e9b64a4cf5c0b1766f98b4eceb2e27061d9
Carles Muiños <carles.ml.dev@gmail.com>
2017-01-13 03:11:09 +0100

    add new webdoc content

--- src/assets/scripts/ui/intent.js
+++ src/assets/scripts/ui/intent.js
@@ -2,21 +2,8 @@ import $ from 'jquery'
 
 export const intent = {
   setup () {
+    // Sidebar
     const $sidebar = $('.ui.sidebar')
-
-    const $cookiesModal = $('.a1.cookies.modal')
-    const $creditsModal = $('.a1.credits.modal')
-
-    const $v1Modal = $('.a1.v1.modal')
-    const $e1Modal = $('.a1.e1.modal')
-    const $e2Modal = $('.a1.e2.modal')
-    const $v2Modal = $('.a1.v2.modal')
-
-    const $v1 = $v1Modal.find('.ui.embed').first()
-    const $e1 = $e1Modal.find('.ui.embed').first()
-    const $e2 = $e2Modal.find('.ui.embed').first()
-    const $v2 = $v2Modal.find('.ui.embed').first()
-
     $sidebar
       .sidebar({
         transition: 'overlay',
@@ -26,50 +13,47 @@ export const intent = {
       .sidebar('attach events', '.ui.sidebar .remove.icon')
       .sidebar('attach events', '.a1.plus.cell')
 
+    // Text Modals
+    const $cookiesModal = $('.a1.cookies.modal')
     $cookiesModal
       .modal({
         transition: 'fade down'
       })
       .modal('attach events', '.a1.cookies.item', 'show')
 
+    const $creditsModal = $('.a1.credits.modal')
     $creditsModal
       .modal({
         transition: 'fade down'
       })
       .modal('attach events', '.a1.credits.item', 'show')
 
-    $v1Modal
-      .modal({
-        transition: 'scale',
-        onShow () { $v1.embed() },
-        onHide () { $v1.embed('destroy') }
-      })
-      .modal('attach events', '.a1.v1.floor.cell', 'show')
-      .modal('attach events', '.ui.link.items .a1.v1.item', 'show')
+    // Video Modals
+    const videoModals = [
+      { selector: '.a1.v1.modal', triggers: ['.ui.link.items .a1.v1.item', '.a1.v1.floor.cell'] },
+      { selector: '.a1.e11.modal', triggers: ['.ui.link.items .a1.e11.item'] },
+      { selector: '.a1.e12.modal', triggers: ['.ui.link.items .a1.e12.item'] },
+      { selector: '.a1.v2.modal', triggers: ['.ui.link.items .a1.v2.item', '.a1.v2.floor.cell'] },
+      { selector: '.a1.v3.modal', triggers: ['.ui.link.items .a1.v3.item', '.a1.v3.floor.cell'] },
+      { selector: '.a1.e2.modal', triggers: ['.ui.link.items .a1.e2.item'] },
+      { selector: '.a1.e3.modal', triggers: ['.ui.link.items .a1.e3.item'] },
+      { selector: '.a1.e4.modal', triggers: ['.ui.link.items .a1.e4.item'] }
+    ]
 
-    $e1Modal
-      .modal({
-        transition: 'scale',
-        onShow () { $e1.embed() },
-        onHide () { $e1.embed('destroy') }
-      })
-      .modal('attach events', '.ui.link.items .a1.e1.item', 'show')
+    const setupVideoModal = (modal) => {
+      const $videoModal = $(modal.selector)
+      const $video = $videoModal.find('.ui.embed').first()
 
-    $e2Modal
-      .modal({
-        transition: 'scale',
-        onShow () { $e2.embed() },
-        onHide () { $e2.embed('destroy') }
-      })
-      .modal('attach events', '.ui.link.items .a1.e2.item', 'show')
+      $videoModal
+        .modal({
+          transition: 'scale',
+          onShow () { $video.embed() },
+          onHide () { $video.embed('destroy') }
+        })
 
-    $v2Modal
-      .modal({
-        transition: 'scale',
-        onShow () { $v2.embed() },
-        onHide () { $v2.embed('destroy') }
-      })
-      .modal('attach events', '.a1.v2.floor.cell', 'show')
-      .modal('attach events', '.ui.link.items .a1.v2.item', 'show')
+      modal.triggers.forEach((selector) => $videoModal.modal('attach events', selector, 'show'))
+    }
+
+    videoModals.forEach(setupVideoModal)
   }
 }
